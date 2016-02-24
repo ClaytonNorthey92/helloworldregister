8dd99a6f6cb6494d3977f3cb36d680f30196e0c3
ClaytonNorthey92 <clayton.northey@gmail.com>
2016-02-24 13:54:55 -0500

    deployment stuff

--- registration/registration/wsgi.py
+++ registration/registration/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for registration project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
+# +++++++++++ DJANGO +++++++++++
 import os
+import sys
+
+## assuming your Django settings file is at '/home/my_username/projects/my_project/settings.py'
+path = '/home/cnorthey/projects'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'my_project.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "registration.settings")
-
 application = get_wsgi_application()
